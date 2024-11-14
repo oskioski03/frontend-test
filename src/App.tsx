997424a84059dfb411ab40248ec0b6d27dd20aab
oskioski03 <oskioski03@wp.pl>
2024-11-14 16:23:11 +0100

    edit app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,38 @@ function App() {
     .then((data) => setData(data.data))
   }, [])
 
- console.log('data ', data)
+  const dataArr = [
+    {
+        id: '1',
+        name: 'Adam',
+        surname: "Karolczak"
+    },
+    {
+        id: '2',
+        name: 'Janina',
+        surname: "Grodzka"
+    },
+    {
+        id: '3',
+        name: 'Ziomek',
+        surname: "Bazowy"
+    },
+    {
+        id: '4',
+        name: 'Rizzler',
+        surname: "Sigmowy"
+    },
+    {
+        id: '5',
+        name: 'Skibidi',
+        surname: "Toiletowy"
+    },
+]
 
   return (
     <div style={{display: 'flex', gap: "10px"}} >
       <h1>TESTOWANIE</h1>
-      {!data.length ? 'no data' : data.map((item) => <Card {...item} />)}
+      {!dataArr.length ? 'no data' : dataArr.map((item) => <Card {...item} />)}
     </div>
   )
 }
